21beecd2ba1e330656110affe3a0abfca56a888a
kjthier <karinjthier@gmail.com>
2023-08-16 20:23:54 +0200

    server files

---
+++ server/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes/pokemon.js';  
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use('/pokemon', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
